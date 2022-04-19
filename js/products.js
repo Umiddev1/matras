b065d43ad5e6ded6b9d518af024c9240fe0eb382
Umiddev1 <umidjanrahmanjanov@gmail.com>
2022-04-19 18:05:44 +0500

    end

--- js/products.js
+++ js/products.js
@@ -1,5 +1,6 @@
 let elProductLink = document.querySelectorAll('.products__link');
 let elProductMatrasList = document.querySelector('.products__matras-list');
+let elProductMiniList = document.querySelector('.products__mini-list');
 elProductLink.forEach(item => {
   item.addEventListener("click", e => {
       elProductLink.forEach(btn => btn.classList.remove("active"));
@@ -42,11 +43,10 @@ let bigData = [
     icon:"bx bx-cart"
   },
 ]
-
 for(i of bigData) {
   elProductMatrasList.innerHTML += `<li class="products__matras-item">
   <div class="products__matras-wrapper">
-  <div>
+  <div class="products__imgs">
   <span class="products__matras-sign">${i.sign}</span>
   <img class="products__matras-img" src='${i.img}' alt="img">
   </div>
@@ -56,13 +56,31 @@ for(i of bigData) {
         <li class="products__size-item"><p class="products__size-text">Yuklama</p><p class="products__size-subtext">150 <span class="products__size-spn">kg</span></p></li>
         <li class="products__size-item"><p class="products__size-text">Kafolat</p><p class="products__size-subtext">3 <span class="products__size-spn">yil</span></p></li>
         <li class="products__size-item"><p class="products__size-text">O'lchami</p><p class="products__size-subtext">200x120x40</p></li>
-        <li class="products__size-item"><p class="products__size-text">Sig'imi</p><p class="products__size-subtext">2 <span class="products__size-spn">kishilik</span></p></li>
+        <li class="products__size-item "><p class="products__size-text">Sig'imi</p><p class="products__size-subtext">2 <span class="products__size-spn">kishilik</span></p></li>
       </ul>
       <p class="products__size-texts">Penatibus viverra gravida rhoncus in. At turpis morbi ante tortor a est. Habitant adipiscing ut sed pulvinar tellus, ut urna, fermentum. Porttitor senectus lorem rhoncus facilisi ac dictum varius egestas.</p>
       <p class="product__price-text">Narxi</p>
       <p class="product__price">1 <span>699</span> <span>999</span> <span class="product__valyuta">so'm</span></p>
-      <button class="products__btn">${i.order}<span><i class='${i.icon}'></i></span></button>
+      <button class="products__btn">${i.order} ${i.sale}<span><i class='${i.icon}'></i></span></button>
     </div>
   </div>
   </li>` 
 }
+let miniDatas = [
+  {
+    id:1,
+    sale:"Aksiya",
+    img:"img/mat6.png",
+    title:"Ortopedik Eko matras",
+    order:"Buyurtma",
+    icon:"bx bx-cart"
+  },
+  {
+    id:2,
+    sale:"Aksiya",
+    img:"img/mat5.png",
+    title:"Ortopedik Eko matras",
+    order:"Buyurtma",
+    icon:"bx bx-cart"
+  }
+]
