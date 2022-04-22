e01acd0d0b4ab6119a02b5f2941417b8ea364c75
Umiddev1 <umidjanrahmanjanov@gmail.com>
2022-04-22 07:15:03 +0500

    ор

--- js/products.js
+++ js/products.js
@@ -1,6 +1,8 @@
 let elProductLink = document.querySelectorAll('.products__link');
 let elProductMatrasList = document.querySelector('.products__matras-list');
 let elProductMiniList = document.querySelector('.products__mini-list');
+let elModal = document.querySelector('.modal-bg');
+let elClose = document.querySelector('.close');
 elProductLink.forEach(item => {
   item.addEventListener("click", e => {
       elProductLink.forEach(btn => btn.classList.remove("active"));
@@ -23,7 +25,8 @@ let bigData = [
     img:"img/matras2.png",
     title:"Ortopedik Eko matras",
     order:"Buyurtma",
-    icon:"bx bx-cart"
+    icon:"bx bx-cart",
+    search: "bx bx-search-alt-2"
   },
   {
     id:3,
@@ -49,19 +52,20 @@ for(i of bigData) {
   <div class="products__imgs">
   <span class="products__matras-sign">${i.sign}</span>
   <img class="products__matras-img" src='${i.img}' alt="img">
+  <i class='${i.search}'></i>
   </div>
     <div class="products__matras-des">
       <h2 class="products__matras-title">${i.title}</h2>
       <ul class="products__size-list">
         <li class="products__size-item"><p class="products__size-text">Yuklama</p><p class="products__size-subtext">150 <span class="products__size-spn">kg</span></p></li>
-        <li class="products__size-item"><p class="products__size-text">Kafolat</p><p class="products__size-subtext">3 <span class="products__size-spn">yil</span></p></li>
+        <li class="products__size-item "><p class="products__size-text">Kafolat</p><p class="products__size-subtext">3 <span class="products__size-spn">yil</span></p></li>
         <li class="products__size-item"><p class="products__size-text">O'lchami</p><p class="products__size-subtext">200x120x40</p></li>
-        <li class="products__size-item "><p class="products__size-text">Sig'imi</p><p class="products__size-subtext">2 <span class="products__size-spn">kishilik</span></p></li>
+        <li class="products__size-item"><p class="products__size-text">Sig'imi</p><p class="products__size-subtext">2 <span class="products__size-spn">kishilik</span></p></li>
       </ul>
       <p class="products__size-texts">Penatibus viverra gravida rhoncus in. At turpis morbi ante tortor a est. Habitant adipiscing ut sed pulvinar tellus, ut urna, fermentum. Porttitor senectus lorem rhoncus facilisi ac dictum varius egestas.</p>
       <p class="product__price-text">Narxi</p>
       <p class="product__price">1 <span>699</span> <span>999</span> <span class="product__valyuta">so'm</span></p>
-      <button class="products__btn">${i.order} ${i.sale}<span><i class='${i.icon}'></i></span></button>
+      <button class="products__btn">${i.order}<span><i class='${i.icon}'></i></span></button>
     </div>
   </div>
   </li>` 
@@ -84,3 +88,16 @@ let miniDatas = [
     icon:"bx bx-cart"
   }
 ]
+
+let elBtn = document.querySelector('.bx-search-alt-2');
+
+elBtn.addEventListener("click", handleBtn) 
+elClose.addEventListener("click", handleClose)
+
+function handleBtn() {
+  elModal.classList.add("block");
+}
+
+function handleClose() {
+  elModal.classList.remove("block");
+}
